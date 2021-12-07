afc1a407274a73cbd3dd9ad00134b512aa27b585
hoanghuy1998 <87927778+hoanghuy1998@users.noreply.github.com>
2021-12-07 13:06:43 +0700

    Update connectAllProductMysql.js

--- common/connectAllProductMysql.js
+++ common/connectAllProductMysql.js
@@ -6,7 +6,7 @@ const connectionAllProduct = mysql.createConnection({
   database: "dbcozastore",
 });
 connectionAllProduct.connect((err) => {
-  console.log(err)
+  console.log("err",err)
   if (err) {
     console.log("connect fails");
   } else {
