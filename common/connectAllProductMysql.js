b3b13677b51c615958c8faea13d6262b1b9bbe36
hoanghuy1998 <87927778+hoanghuy1998@users.noreply.github.com>
2021-12-12 18:02:38 +0700

    Update connectAllProductMysql.js

--- common/connectAllProductMysql.js
+++ common/connectAllProductMysql.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql");
 const connectionAllProduct = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "dbcozastore",
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "dbcozastore",
   // database server
-  // host: "sql6.freesqldatabase.com",
-  // user: "sql6456957",
-  // password: "vzYvFN3QQF",
-  // database: "sql6456957",
+  host: "sql6.freesqldatabase.com",
+  user: "sql6456957",
+  password: "vzYvFN3QQF",
+  database: "sql6456957"
 });
 connectionAllProduct.connect((err) => {
   if (err) {
